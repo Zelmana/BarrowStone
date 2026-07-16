9ea62a3469823e0d8b3ca70d8cb2241d27ddbfe3
Zelmana <zerjar@gmail.com>
2026-07-15 23:24:22 -0400

    Quartz sync: Jul 15, 2026, 11:24 PM

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -16,7 +16,7 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
 
 PageTitle.css = `
 .page-title {
-  font-size: 2.2rem;
+  font-size: 1.8rem;
   margin: 0;
   display: flex;
   align-items: center;
